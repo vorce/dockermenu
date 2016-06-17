ee83f002ce665c1f06ece1e013a2bed6ba9e6fcf
Joel Carlbark <joel.carlbark@meltwater.com>
2016-06-17 10:03:25 +0200

    Initial Commit

---
+++ dockermenu/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  dockermenu
+//
+//  Created by Joel Carlbark on 2016-06-17.
+//  Copyright © 2016 Joel Carlbark. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
